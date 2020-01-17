d01b62942ec78d9115ce60c73778cb92c9483d29
Rus Hughes <russell.hughes@gmail.com>
2020-01-17 17:54:36 +0100

    cleaned up profile html

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -20,14 +20,14 @@
       </div>
       <div class="card-stacked">
         <div class="card-content">
-        <p>
-          name: <strong>{{ user.displayName }}</strong><br />email:<strong>{{ user.email }}</strong><br/>uid:
-                  <strong>{{ user.uid }}</strong> <br />provider:
-                  <strong
-                    class="teal-text"
-                  >
-                    {{ user.providerData[0].providerId }}
-                  </strong>
+          <p>
+            name: <strong>{{ user.displayName }}</strong>
+            <br>
+            email: <strong>{{ user.email }}</strong>
+            <br>
+            uid: <strong>{{ user.uid }}</strong>
+            <br>
+            provider: <strong class="teal-text">{{ user.providerData[0].providerId }}</strong>
           </p>
         </div>
       </div>
