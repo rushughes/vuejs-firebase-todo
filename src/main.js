6e09900220c05c3d82e21d2e9e0ce4b24c4d7df4
Rus Hughes <russell.hughes@gmail.com>
2020-01-17 17:05:49 +0100

    working base app with firebase

--- src/main.js
+++ src/main.js
@@ -1,8 +1,39 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Firebase from 'firebase';
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App';
+import Home from './components/Home';
+import Login from './components/Login';
+import Profile from './components/Profile';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_APIKEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASEURL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.VUE_APP_FIREBASE_APPID,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENTID
+};
+// Initialize Firebase
+Firebase.initializeApp(firebaseConfig);
+Firebase.analytics();
 
 Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/', component: Home },
+    { path: '/profile', component: Profile },
+    { path: '/login', component: Login },
+  ]
+});
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
